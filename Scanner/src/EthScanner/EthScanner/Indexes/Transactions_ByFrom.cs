d5065c5b6481549596143b84900a6962261f284e
Dejan Milicic <dejan.milicic@gmail.com>
2021-09-08 15:15:44 +0200

    Transactions_ByFrom

---
+++ Scanner/src/EthScanner/EthScanner/Indexes/Transactions_ByFrom.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using EthScanner.Models;
+using Raven.Client.Documents.Indexes;
+
+namespace EthScanner.Indexes
+{
+    public class Transactions_ByFrom : AbstractIndexCreationTask<TransactionInfo, Transactions_ByFrom.Entry>
+    {
+        public class Entry
+        {
+            public string From { get; set; }
+
+            public int Transactions { get; set; }
+
+            public decimal Ether { get; set; }
+        }
+
+        public Transactions_ByFrom()
+        {
+            Map = transactions => from trx in transactions
+                select new Entry
+                {
+                    From = trx.From,
+                    Transactions = 1,
+                    Ether = trx.Ether
+                };
+
+            Reduce = results => from result in results
+                group result by result.From
+                into g
+                select new Entry
+                {
+                    From = g.Key,
+                    Transactions = g.Sum(x => x.Transactions),
+                    Ether = g.Sum(x => x.Ether)
+                };
+        }
+    }
+}
