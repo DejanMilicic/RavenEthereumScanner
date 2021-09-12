a479b35bebf306581e1349abb22b5403072ddee7
Dejan Milicic <dejan.milicic@gmail.com>
2021-09-12 22:26:53 +0200

    Deploy indexes on startup

--- Scanner/src/EthScanner/EthScanner/Startup.cs
+++ Scanner/src/EthScanner/EthScanner/Startup.cs
@@ -51,8 +51,10 @@ namespace EthScanner
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDocumentStore store)
         {
+            using var session = store.OpenSession();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
